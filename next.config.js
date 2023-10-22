2ea28718c47e8c9676017b5f8ca7430fef920eef
prashantpaddune <padadunep@gmail.com>
2023-10-22 17:21:20 +0530

    chore: fix cart icon
    Signed-off-by: prashantpaddune <padadunep@gmail.com>

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
         protocol: 'http',
         hostname: 'books.google.com'
       },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com'
+      },
     ],
   },
 }
